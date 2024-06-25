f8bc1b2b9ab3629b48440c3e1beed27216be5846
otaviob <otaviob22@gmail.com>
2024-06-26 00:59:21 +1000

    Initial Commit

---
+++ ios-tableview-customcell/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ios-tableview-customcell
+//
+//  Created by Otavio Brito on 26/6/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
