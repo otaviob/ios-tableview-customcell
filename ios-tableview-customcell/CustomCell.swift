6ba0b05298f19e7f8ac0811f96567d377de586a1
otaviob <otaviob22@gmail.com>
2024-07-05 16:43:12 +1000

    customcell config

---
+++ ios-tableview-customcell/CustomCell.swift
@@ -0,0 +1,41 @@
+//
+//  CustomCell.swift
+//  ios-tableview-customcell
+//
+//  Created by Otavio Brito on 5/7/2024.
+//
+
+import UIKit
+
+class CustomCell: UITableViewCell {
+
+    static let identifier = "CustomCell"
+    
+    private let myImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.contentMode = .scaleAspectFit
+        iv.image = UIImage(systemName: "questionmark")
+        iv.tintColor = .label
+        return iv
+    }()
+    
+    private let myLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .label
+        label.textAlignment = .left
+        label.font = .systemFont(ofSize: 24, weight: .medium)
+        label.text = "Error"
+        return label
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+
+}
--- ios-tableview-customcell/ViewController.swift
+++ ios-tableview-customcell/ViewController.swift
@@ -8,12 +8,80 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // MARK: - Variables
+    
+    private let images: [UIImage] = [
+        UIImage(named: "1")!,
+        UIImage(named: "2")!,
+        UIImage(named: "3")!,
+        UIImage(named: "4")!,
+        UIImage(named: "5")!,
+    
+    
+    
+    ]
+    
+    
+    // MARK: - UI Components
+    
+    private let tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.allowsSelection = true
+        tableView.register(CustomCell.self, forCellReuseIdentifier: CustomCell.identifier)
+        return tableView
+    }()
+    
+    
+    
+    
+    
+    // MARK: - Lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemRed
+        self.setupUI()
+        
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+    }
+    
+    // MARK: Setup UI
+    
+    private func setupUI() {
+        self.view.backgroundColor = .systemBlue
+        
+        self.view.addSubview(tableView)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: self.view.topAnchor),
+            tableView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor),
+            tableView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor)
+                                    
+        ])
     }
 
 
+}
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.images.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: CustomCell.identifier, for: indexPath) as? CustomCell else {
+            fatalError("The TableView could not dequeue a CustomCell in ViewController.")
+            
+        }
+        
+        cell.textLabel?.text = indexPath.row.description
+        
+        return cell
+        
+    }
 }
 
